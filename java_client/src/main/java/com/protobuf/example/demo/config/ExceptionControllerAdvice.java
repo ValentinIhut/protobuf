27ac31df126e46c45a4da71dc6cd51b75c88b6b8
Valentin Ihut <valentin.ihut@gmail.com>
2018-06-21 00:12:52 +0300

    added javaDoc for project

--- java_client/src/main/java/com/protobuf/example/demo/config/ExceptionControllerAdvice.java
+++ java_client/src/main/java/com/protobuf/example/demo/config/ExceptionControllerAdvice.java
@@ -11,9 +11,13 @@ import org.springframework.web.context.request.WebRequest;
  * @author valentin.ihut
  * @since 1.0.0
  */
+
 @RestControllerAdvice
 public class ExceptionControllerAdvice {
 
+  /*
+  Controller advice for setting the proper http response code if an error occurs.
+   */
   @ExceptionHandler({BusinessException.class})
   protected ResponseEntity<Object> handleConflict(BusinessException e, WebRequest request) {
     return new ResponseEntity<>(e, null, HttpStatus.INTERNAL_SERVER_ERROR);
